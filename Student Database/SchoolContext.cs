2630bbe56c403e44913c8059484faec95a24fdf8
Amritpalkaur18 <palamrit0218@gmail.com>
2024-10-29 18:17:59 -0400

    Add project files.

---
+++ Student Database/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Data.Entity;
+public class SchoolContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+}
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SchoolContext())
+        {
+            var student = new Student
+            {
+                Name = "John Doe",
+                EnrollmentDate = DateTime.Now
+            };
+
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully!");
+        }
+    }
+}
